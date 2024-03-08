290a9528b3670d736833397764aa5fd2c5f5db8f
Saikat Roy <saikatroydot@gmail.com>
2024-03-08 13:15:09 +0530

    navbar complete

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'library'
+    'library',
+    'django_bootstrap_icons',
+    'django_sass',
 ]
 
 MIDDLEWARE = [
