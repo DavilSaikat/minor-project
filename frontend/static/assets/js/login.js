503a02a5a130f42c39ea31b6a3810ea3411048f7
Saikat Roy <saikatroydot@gmail.com>
2024-04-16 10:42:44 +0530

    animated login page

--- frontend/static/assets/js/login.js
+++ frontend/static/assets/js/login.js
@@ -1,9 +1,17 @@
 $(".signup-link").click(function () {
   $(".wrapper").addClass("animated-signin");
   $(".wrapper").removeClass("animated-signup");
+  $(".wrapper").removeClass("animated-forgot");
 });
 
 $(".signin-link").click(function () {
   $(".wrapper").addClass("animated-signup");
   $(".wrapper").removeClass("animated-signin");
+  $(".wrapper").removeClass("animated-forgot");
+});
+
+$(".forgot-btn").click(function () {
+  $(".wrapper").addClass("animated-forgot");
+  $(".wrapper").removeClass("animated-signin");
+  $(".wrapper").removeClass("animated-signup");
 });
